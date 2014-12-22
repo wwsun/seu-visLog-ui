5af262fb9c6d495c917a16242255513108862f16
Weiwei <ww.sun@outlook.com>
2014-12-22 15:32:51 +0800

    make the nodes clickable

--- js/pages/link.js
+++ js/pages/link.js
@@ -2,7 +2,6 @@
  * Created by Weiwei on 12/15/2014.
  */
 
-
 var linkPage = {
     overviewForceChart : echarts.init(document.getElementById('svg-wrapper')),
     subForceChart : echarts.init(document.getElementById('path-detail')),
@@ -85,9 +84,7 @@ var linkPage = {
         ]
     },
 
-    subForceChartOption : {
-
-    },
+    subForceChartOption : { },
 
     visitLineOption : {
         tooltip : {
@@ -182,12 +179,6 @@ var linkPage = {
 
 };
 
-//var myChart = echarts.init(document.getElementById('svg-wrapper'));
-//var pathDetailChart = echarts.init(document.getElementById('path-detail'));
-//var clickFlowChart = echarts.init(document.getElementById('click-flow'));
-//var jumpingChart = echarts.init(document.getElementById('jumping'));
-//var landingSourceChart = echarts.init(document.getElementById('landing-source'));
-
 $.getJSON('./data/demo.json', function (json) {
 
     linkPage.overviewForceOption.series[0].nodes = json.nodes;
@@ -200,12 +191,7 @@ $.getJSON('./data/demo.json', function (json) {
 
     setDegreePie();
 
-    //linkPage.degreePieChart.setOption(linkPage.degreePieOption);
-    //clickFlowChart.setOption(lineOptions);
-    //jumpingChart.setOption(pieOptions);
-    //landingSourceChart.setOption(barOptions);
-
-
+    linkPage.subForceChart.on(echarts.config.EVENT.CLICK, focus);
 });
 
 function setDegreePie() {
@@ -260,224 +246,23 @@ function setDegreePie() {
     });
 }
 
-
-//function focus(param) {
-//    var data = param.data;
-//    var links = options.series[0].links;
-//    var nodes = options.series[0].nodes;
-//    if (
-//        data.source !== undefined
-//        && data.target !== undefined
-//    ) { //点击的是边
-//        var sourceNode = nodes[data.source];
-//        var targetNode = nodes[data.target];
-//        console.log("选中了边 " + sourceNode.name + ' -> ' + targetNode.name + ' (' + data.weight + ')');
-//    } else { // 点击的是点
-//        console.log("选中了" + data.name + '(' + data.value + ')');
-//    }
-//}
+function focus(param) {
+    var data = param.data;
+    //var links = linkPage.subForceChartOption.series[0].links;
+    var nodes = linkPage.overviewForceOption.series[0].nodes;
+    if (
+        data.source !== undefined
+        && data.target !== undefined
+    ) {
+        var sourceNode = nodes[data.source];
+        var targetNode = nodes[data.target];
+        console.log("选中了边 " + sourceNode.name + ' -> ' + targetNode.name + ' (' + data.weight + ')');
+    } else {
+        console.log("选中了" + data.name + '(' + data.value + ')');
+    }
+}
 
 //myChart.on(echarts.config.EVENT.CLICK, focus);
 //myChart.on(echarts.config.EVENT.FORCE_LAYOUT_END, function () {
 //    console.log(myChart.chart.force.getPosition());
-//});
-
-var lineOptions = {
-    tooltip : {
-        trigger: 'axis'
-    },
-    toolbox: {
-        show : false
-    },
-    calculable : true,
-    xAxis : [
-        {
-            type : 'category',
-            boundaryGap : false,
-            data : ['-7','-6','-5','-3','-2','-1','0']
-        }
-    ],
-    yAxis : [
-        {
-            type : 'value',
-            axisLabel : {
-                formatter: '{value}'
-            }
-        }
-    ],
-    series : [
-        {
-            name:'访问趋势',
-            type:'line',
-            data:[11, 11, 15, 22, 12, 13, 10],
-            markPoint : {
-                data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name: '平均值'}
-                ]
-            }
-        }
-    ]
-};
-
-var pieOptions = {
-    tooltip : {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        orient : 'vertical',
-        x : 'left',
-        data:['跳入','跳出']
-    },
-    toolbox: {
-        show : false
-    },
-    calculable : true,
-    series : [
-        {
-            name:'页面跳入跳出',
-            type:'pie',
-            radius : ['50%', '70%'],
-            itemStyle : {
-                normal : {
-                    label : {
-                        show : false
-                    },
-                    labelLine : {
-                        show : false
-                    }
-                },
-                emphasis : {
-                    label : {
-                        show : true,
-                        position : 'center',
-                        textStyle : {
-                            fontSize : '30',
-                            fontWeight : 'bold'
-                        }
-                    }
-                }
-            },
-            data:[
-                {value:200, name:'跳入'},
-                {value:50, name:'跳出'}
-            ]
-        }
-    ]
-};
-
-var barOptions = {
-    tooltip : {
-        trigger: 'axis'
-    },
-    legend: {
-        data:['页面来源']
-    },
-    toolbox: {
-        show : false
-    },
-    calculable : true,
-    xAxis : [
-        {
-            type : 'category',
-            data : ['首页','产品目录A','产品目录B','搜索引擎','活动宣传','站内搜索']
-        }
-    ],
-    yAxis : [
-        {
-            type : 'value'
-        }
-    ],
-    series : [
-        {
-            name:'页面来源',
-            type:'bar',
-            data:[100, 20, 10, 4, 60, 20, 30],
-            markPoint : {
-                data : [
-                    {type : 'max', name: '最大值'},
-                    {type : 'min', name: '最小值'}
-                ]
-            },
-            markLine : {
-                data : [
-                    {type : 'average', name: '平均值'}
-                ]
-            }
-        }
-    ]
-};
-
-//myChart.setOption({
-//    tooltip : {
-//        trigger: 'axis'
-//    },
-//    legend: {
-//        data:['蒸发量','降水量']
-//    },
-//    toolbox: {
-//        show : true,
-//        feature : {
-//            mark : {show: true},
-//            dataView : {show: true, readOnly: false},
-//            magicType : {show: true, type: ['line', 'bar']},
-//            restore : {show: true},
-//            saveAsImage : {show: true}
-//        }
-//    },
-//    calculable : true,
-//    xAxis : [
-//        {
-//            type : 'category',
-//            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
-//        }
-//    ],
-//    yAxis : [
-//        {
-//            type : 'value',
-//            splitArea : {show : true}
-//        }
-//    ],
-//    series : [
-//        {
-//            name:'蒸发量',
-//            type:'bar',
-//            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-//        },
-//        {
-//            name:'降水量',
-//            type:'bar',
-//            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-//        }
-//    ]
-//});
-
-// --- 地图 ---
-//var myChart2 = echarts.init(document.getElementById('mainMap'));
-//myChart2.setOption({
-//    tooltip : {
-//        trigger: 'item',
-//        formatter: '{b}'
-//    },
-//    series : [
-//        {
-//            name: '中国',
-//            type: 'map',
-//            mapType: 'china',
-//            selectedMode : 'multiple',
-//            itemStyle:{
-//                normal:{label:{show:true}},
-//                emphasis:{label:{show:true}}
-//            },
-//            data:[
-//                {name:'广东',selected:true}
-//            ]
-//        }
-//    ]
 //});
